daea684fb41b2e180925e444fe72828eeb3a5827
Simon Scarfe <simon.scarfe@gmail.com>
2015-11-28 00:28:58 +0000

    living clojure week 4 day 3 - encode passes. Eek.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,33 @@
 (ns alphabet-cipher.coder)
 
+(defn rotate [n l] (concat (drop n l) (take n l)))
+
+(def alpha-start (int \a))
+
+(def substitution-chart
+  (let [alphabet (map char (range alpha-start (inc (int \z))))]
+  (for [row alphabet]
+    (rotate
+      (- (int row) alpha-start)
+      alphabet))))
+
+(defn encode-char [x y]
+  (nth (nth substitution-chart (- (int x) alpha-start))
+    (- (int y) alpha-start)))
+
+(defn normalise-keyword [keyword len]
+  (let [repetitions (inc (quot len (count keyword)))]
+    (apply str
+           (take len
+                 (apply str (repeat repetitions keyword))))))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str (map encode-char
+                  (normalise-keyword keyword (count message))
+                  message)))
 
 (defn decode [keyword message]
   "decodeme")
 
 (defn decipher [cipher message]
   "decypherme")
-
